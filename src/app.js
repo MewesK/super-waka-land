4072a3e954783c3eb791210231b8cdb713d4d481
Mewes Kochheim <kochheim@develop4edu.de>
2022-05-27 09:16:01 +0200

    WIP

--- src/app.js
+++ src/app.js
@@ -1,30 +1,72 @@
-import * as PIXI from "pixi.js";
+import {
+  AnimatedSprite,
+  Application,
+  Loader,
+  ObservablePoint,
+  Point,
+  Sprite,
+  Spritesheet,
+  Ticker,
+  settings,
+  SCALE_MODES,
+} from "pixi.js";
+import { CompositeTilemap } from "@pixi/tilemap";
 
 // Import ressources
 import ratAtlas from "./images/atlas/rat.json";
-import ratAtlasImage from "./images/atlas/rat.png";
-
-// Define aliases
-const loader = PIXI.Loader.shared;
+import ratSprites from "./images/atlas/rat.png";
 
 // Settings
-PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+settings.SCALE_MODE = SCALE_MODES.NEAREST;
+
+const resolutionX = 256;
+const resolutionY = 224;
+const tileSizeX = 16;
+const tileSizeY = 16;
+const numberOfTiles = parseInt(resolutionX / tileSizeX) * 2;
+
+// Variables
+const world = {
+  elapsed: 0.0,
+  gravity: 1,
+  startFloorY: 12,
+};
+
+const player = {
+  power: 1.0,
+  mass: 1.0,
+  force: new Point(2, 0),
+  velocity: new Point(0, 0),
+  position: new Point(2 * tileSizeX, world.startFloorY * tileSizeY - 32),
+};
+
+// Sprites
+let backgroundSprite;
+let coinSprite;
+let ratWalkSprite;
+let ratRunSprite;
+let ratJumpSprite;
+let groundTiles;
 
 // Create a Pixi application
-const app = new PIXI.Application({
-  width: 256,
-  height: 224,
+const app = new Application({
+  width: resolutionX,
+  height: resolutionY,
   antialias: false,
-  resolution: 2,
+  resolution: 3,
+  backgroundColor: 0xffffde,
 });
-app.renderer.backgroundColor = 0x061639;
 
 // Mount application
 document.getElementById("app").appendChild(app.view);
 
+// Register event listeners
+app.view.addEventListener("click", jump);
+app.view.addEventListener("touchend", jump);
+
 // Load ressources
-loader.onProgress.add(loadProgressHandler);
-loader.add("ratAtlasImage", ratAtlasImage).load(setup);
+Loader.shared.onProgress.add(loadProgressHandler);
+Loader.shared.add(ratSprites).load(setup);
 
 function loadProgressHandler(loader, resource) {
   console.log("Loading: " + resource.url);
@@ -35,38 +77,86 @@ function setup() {
   console.log("All files loaded");
 
   // Load spritesheet
-  const texture = loader.resources["ratAtlasImage"].texture.baseTexture;
-  const sheet = new PIXI.Spritesheet(texture, ratAtlas);
+  const sheet = new Spritesheet(
+    Loader.shared.resources[ratSprites].texture.baseTexture,
+    ratAtlas
+  );
   sheet.parse(() => {
-    const backgroundSprite = PIXI.Sprite.from("bg_wakaland.png"); 
-      
-    const ratWalkSprite = PIXI.AnimatedSprite.fromFrames([
-      "rat_idle.png",
-      "rat_walk.png",
+    console.log("Spritesheet loaded");
+
+    // Create sprites
+    backgroundSprite = Sprite.from("bg_wakaland.png");
+
+    coinSprite = AnimatedSprite.fromFrames([
+      "coin1.png",
+      "coin2.png",
+      "coin3.png",
+      "coin4.png",
     ]);
+    coinSprite.animationSpeed = 0.15;
+    coinSprite.x = 190;
+    coinSprite.y = 127;
+    coinSprite.play();
+
+    ratWalkSprite = AnimatedSprite.fromFrames(["rat_idle.png", "rat_walk.png"]);
     ratWalkSprite.animationSpeed = 0.1;
-    ratWalkSprite.x = 142;
-    ratWalkSprite.y = 133;
+    ratWalkSprite.x = player.position.x;
+    ratWalkSprite.y = player.position.y;
     ratWalkSprite.play();
 
-    const ratRunSprite = PIXI.AnimatedSprite.fromFrames([
-      "rat_idle.png",
-      "rat_run.png",
-    ]);
+    ratRunSprite = AnimatedSprite.fromFrames(["rat_idle.png", "rat_run.png"]);
     ratRunSprite.animationSpeed = 0.15;
-    ratRunSprite.x = 190;
-    ratRunSprite.y = 127;
+    ratRunSprite.x = player.position.x;
+    ratRunSprite.y = player.position.y;
     ratRunSprite.play();
 
-    const ratJumpSprite = PIXI.Sprite.from("rat_jump.png");
+    ratJumpSprite = Sprite.from("rat_jump.png");
+    ratJumpSprite.x = player.position.x;
+    ratJumpSprite.y = player.position.y;
 
-    // Add the rocket to the stage
+    // Create tilemap
+    groundTiles = new CompositeTilemap(0, "block.png");
+    drawTiles();
+
+    // Compose stage
     app.stage.addChild(backgroundSprite);
-    app.stage.addChild(ratWalkSprite);
+    app.stage.addChild(groundTiles);
     app.stage.addChild(ratRunSprite);
-    //app.stage.addChild(ratJumpSprite);
 
     // Render the stage
     app.renderer.render(app.stage);
+
+    // Start game loop
+    console.log("Starting game loop");
+    app.ticker.add(gameLoop);
   });
+}
+
+function drawTiles() {
+  console.log("drawTiles");
+  for (var x = 0; x <= numberOfTiles; x++) {
+    groundTiles.addFrame(
+      "block.png",
+      x * tileSizeX,
+      world.startFloorY * tileSizeY
+    );
+  }
+  groundTiles.position.set(player.position.x, groundTiles.position.y);
+}
+
+function gameLoop(delta) {
+  if (Math.floor(player.position.x % resolutionX) === 0) {
+    drawTiles();
+  }
+  player.velocity.x += (player.force.x / player.mass) * delta;
+  player.velocity.y += (player.force.y / player.mass) * delta;
+  player.position.x += player.velocity.x * delta;
+  player.position.y += player.velocity.y * delta;
+  world.elapsed += delta;
+  ratRunSprite.position.set(ratRunSprite.x, player.position.y);
+  groundTiles.pivot.set(player.position.x, groundTiles.position.y);
+}
+
+function jump() {
+  player.velocity.y = -player.power;
 }
