d1d3c6795ddd61a89194b1cce84217682ee0af40
Mewes Kochheim <kochheim@develop4edu.de>
2022-06-09 22:41:13 +0200

    Fixed background reset for infinite scroll

--- src/game/Background.js
+++ src/game/Background.js
@@ -1,6 +1,11 @@
 import { Container, Sprite } from 'pixi.js';
 
 export default class Background {
+  ISLAND_Y = 96;
+  WAVE_Y = 32;
+  ISLAND_SPEED = 0.2 * 20;
+  WAVE_SPEED = 0.25 * 20;
+
   game;
   container = new Container();
 
@@ -17,18 +22,18 @@ export default class Background {
     this.background1Sprite = Sprite.from('bg_wakaland_1');
 
     this.background2aSprite = Sprite.from('bg_wakaland_2');
-    this.background2aSprite.y = this.game.app.screen.height - 96;
+    this.background2aSprite.y = this.game.app.screen.height - this.ISLAND_Y;
 
     this.background2bSprite = Sprite.from('bg_wakaland_2');
     this.background2bSprite.x = this.background2bSprite.width;
-    this.background2bSprite.y = this.game.app.screen.height - 96;
+    this.background2bSprite.y = this.game.app.screen.height - this.ISLAND_Y;
 
     this.background3aSprite = Sprite.from('bg_wakaland_3');
-    this.background3aSprite.y = this.game.app.screen.height - 32;
+    this.background3aSprite.y = this.game.app.screen.height - this.WAVE_Y;
 
     this.background3bSprite = Sprite.from('bg_wakaland_3');
     this.background3bSprite.x = this.background3bSprite.width;
-    this.background3bSprite.y = this.game.app.screen.height - 32;
+    this.background3bSprite.y = this.game.app.screen.height - this.WAVE_Y;
 
     this.container.addChild(this.background1Sprite);
     this.container.addChild(this.background2aSprite);
@@ -46,20 +51,19 @@ export default class Background {
   }
 
   update(dt) {
-    // Parallax scrolling
-    this.background2aSprite.pivot.x += 0.2 * dt;
+    this.background2aSprite.pivot.x += this.ISLAND_SPEED * dt;
     if (this.background2aSprite.pivot.x >= this.game.app.screen.width) {
       this.background2aSprite.pivot.x = 0;
     }
-    this.background2bSprite.pivot.x += 0.2 * dt;
-    if (this.background2aSprite.pivot.x >= this.game.app.screen.width) {
-      this.background2aSprite.pivot.x = 0;
+    this.background2bSprite.pivot.x += this.ISLAND_SPEED * dt;
+    if (this.background2bSprite.pivot.x >= this.game.app.screen.width) {
+      this.background2bSprite.pivot.x = 0;
     }
-    this.background3aSprite.pivot.x += 0.25 * dt;
+    this.background3aSprite.pivot.x += this.WAVE_SPEED * dt;
     if (this.background3aSprite.pivot.x >= this.game.app.screen.width) {
       this.background3aSprite.pivot.x = 0;
     }
-    this.background3bSprite.pivot.x += 0.25 * dt;
+    this.background3bSprite.pivot.x += this.WAVE_SPEED * dt;
     if (this.background3bSprite.pivot.x >= this.game.app.screen.width) {
       this.background3bSprite.pivot.x = 0;
     }
